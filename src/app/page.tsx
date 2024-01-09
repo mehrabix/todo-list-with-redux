c33eff666f8d86f00a05e973dcec6c2d565427e4
mehrabix <mehrabi@post.com>
2024-01-09 13:35:28 +0330

    change provider position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,19 @@ import TodoList from '@/components/TodoList';
 
 export default function Home() {
   return (
-    <Provider store={store}>
-      <div className="container mx-auto mt-8 px-7 md:w-6/12">
-        <Head>
-          <title>Todo App</title>
-          <meta name="description" content="Todo App using Next.js, Redux Toolkit, and Tailwind CSS" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <div className='mx-auto text-center'>
+    <div className="container mx-auto mt-8 px-7 md:w-6/12">
+      <Head>
+        <title>Todo App</title>
+        <meta name="description" content="Todo App using Next.js, Redux Toolkit, and Tailwind CSS" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className='mx-auto text-center'>
         <h1 className="text-4xl font-bold mb-4">Todo App</h1>
-        <AddTodo />
-        <TodoList />
-        </div>
+        <Provider store={store}>
+          <AddTodo />
+          <TodoList />
+        </Provider>
       </div>
-    </Provider>
+    </div>
   )
 }
