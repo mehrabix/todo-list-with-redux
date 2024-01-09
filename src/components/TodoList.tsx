186c16bc811745f63e03906676fe685eee96e776
mehrabix <mehrabi@post.com>
2024-01-09 13:14:09 +0330

    check array length

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,6 @@
-import { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store';
-import { useMemo } from 'react';
 import Todo from './Todo';
 
 const TodoList: React.FC = () => {
@@ -9,20 +8,21 @@ const TodoList: React.FC = () => {
   const [sortByCompleted, setSortByCompleted] = useState<boolean>(false);
 
   const sortedTodos = useMemo(() => {
-    if (sortByCompleted) {
-      return [...todos].sort((a, b) => (a.completed === b.completed ? 0 : a.completed ? -1 : 1));
-    } else {
-      return [...todos].sort((a, b) => a.createdAt - b.createdAt);
-    }
+    const sorted = [...todos].sort((a, b) => (a.completed === b.completed ? 0 : a.completed ? -1 : 1));
+    return sorted;
   }, [todos, sortByCompleted]);
 
   return (
     <div>
       <div className='flex flex-wrap justify-center my-5 gap-4'>
-        <button className='bg-slate-300 px-2 py-1 rounded-md' onClick={() => setSortByCompleted(false)}>Sort by Creation Time</button>
-        <button className='bg-slate-300 px-2 py-1 rounded-md' onClick={() => setSortByCompleted(true)}>Sort by Completion Status</button>
+        <button className='bg-slate-300 px-2 py-1 rounded-md' onClick={() => setSortByCompleted(false)}>
+          Sort by Creation Time
+        </button>
+        <button className='bg-slate-300 px-2 py-1 rounded-md' onClick={() => setSortByCompleted(true)}>
+          Sort by Completion Status
+        </button>
       </div>
-      {sortedTodos.map((todo) => (
+      {sortedTodos.length > 0 && sortedTodos.map((todo) => (
         <Todo key={todo.id} todo={todo} />
       ))}
     </div>
