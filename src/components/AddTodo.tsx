c3c72494df05d42a8f249970b4cfd4cdf4ba3543
mehrabix <mehrabi@post.com>
2024-01-09 08:01:04 +0330

    add todo function useCallback

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,22 +1,24 @@
-'use client'
 import { addTodo } from '@/store/slices/todoSlice';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-
 
 const AddTodo: React.FC = () => {
   const dispatch = useDispatch();
   const [text, setText] = useState<string>('');
 
-  const handleAddTodo = () => {
+  const handleAddTodo = useCallback(() => {
     if (text.trim() !== '') {
-      dispatch(addTodo({
-        id: Date.now(), text, completed: false,
-        createdAt: undefined
-      }));
+      dispatch(
+        addTodo({
+          id: Date.now(),
+          text,
+          completed: false,
+          createdAt: undefined,
+        })
+      );
       setText('');
     }
-  };
+  }, [dispatch, text]);
 
   return (
     <div className="mt-4 flex flex-col">
@@ -27,7 +29,12 @@ const AddTodo: React.FC = () => {
         placeholder="Enter todo"
         className="p-2 border"
       />
-      <button className='bg-green-800 hover:bg-green-700 transition-all duration-200 rounded-md text-white py-2 w-max px-4 mx-auto mt-2' onClick={handleAddTodo}>Add Todo</button>
+      <button
+        className="bg-green-800 hover:bg-green-700 transition-all duration-200 rounded-md text-white py-2 w-max px-4 mx-auto mt-2"
+        onClick={handleAddTodo}
+      >
+        Add Todo
+      </button>
     </div>
   );
 };
